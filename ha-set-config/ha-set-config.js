a16dedfa3e311350139980fcba69423c4e9a81c9
nejcokorn <nejc.okorn@gmail.com>
2026-02-01 18:01:38 +0100

    Adding functionality to set-config

--- ha-set-config/ha-set-config.js
+++ ha-set-config/ha-set-config.js
@@ -4,6 +4,7 @@ module.exports = function(RED) {
 		
 		this.agent = config.agent;
 		this.agentNode = RED.nodes.getNode(config.agent);
+		this.deviceId = config.deviceId;
 		this.inputPort = config.inputPort;
 		this.debounce = config.debounce;
 		this.doubleclick = config.doubleclick;
@@ -18,26 +19,39 @@ module.exports = function(RED) {
 				if (!this.agentNode) {
 					return done(Error("Configure Home Automation agent."));
 				}
+
+				console.log(JSON.stringify([{
+							inputPortIdx: this.inputPort && !Number.isNaN(Number(this.inputPort)) ? Number(this.inputPort) : 0,
+							debounce: this.debounce && !Number.isNaN(Number(this.debounce)) ? Number(this.debounce) : 0,
+							doubleclick: this.doubleclick && !Number.isNaN(Number(this.doubleclick)) ? Number(this.doubleclick) : 0,
+							actions: msg.payload,
+							bypassInstantly: this.bypassInstantly == 1 ? 1 : 0,
+							bypassOnDisconnect: this.bypassOnDisconnect == 1 ? 1 : 0
+						}]));
 				
+
 				const response = await fetch(`${this.agentNode.url}/can/${this.agentNode.canbus}/device/${this.deviceId}/config`,
 					{
 						method: "POST",
 						headers: {
 							"Content-Type": "application/json"
 						},
-						body: JSON.stringify({
+						body: JSON.stringify([{
 							inputPortIdx: this.inputPort && !Number.isNaN(Number(this.inputPort)) ? Number(this.inputPort) : 0,
-							debounce: this.inputPortIdx && !Number.isNaN(Number(this.inputPortIdx)) ? Number(this.inputPortIdx) : 0,
-							doubleclick: this.inputPortIdx && !Number.isNaN(Number(this.inputPortIdx)) ? Number(this.inputPortIdx) : 0,
+							debounce: this.debounce && !Number.isNaN(Number(this.debounce)) ? Number(this.debounce) : 0,
+							doubleclick: this.doubleclick && !Number.isNaN(Number(this.doubleclick)) ? Number(this.doubleclick) : 0,
 							actions: msg.payload,
-							bypassInstantly: this.bypassInstantly,
-							bypassOnDisconnect: this.bypassOnDisconnect,
-						})
+							bypassInstantly: this.bypassInstantly == 1 ? 1 : 0,
+							bypassOnDisconnect: this.bypassOnDisconnect == 1 ? 1 : 0
+						}])
 					}
 				);
 				const json = await response.json();
-
-				send({ payload: json.data });
+				if (json.success == true) {
+					send({ payload: json.data });
+				} else {
+					done(json.error);
+				}
 			} catch (error) {
 				done(error);
 			}
